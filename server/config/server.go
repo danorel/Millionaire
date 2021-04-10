0ba0decabd90633d24063b427a8f2909670114eb
danorel <mail.ordan@gmail.com>
2021-04-10 21:50:39 +0300

    [Config] Created the server config file with reading env variables.

---
+++ server/config/server.go
@@ -0,0 +1,34 @@
+package config
+
+import "os"
+
+type ServerConfig struct {
+	protocol string
+	host string
+	port string
+	addr string
+}
+
+func (c *ServerConfig) Protocol() string {
+	return c.protocol
+}
+func (c *ServerConfig) Host() string {
+	return c.host
+}
+func (c *ServerConfig) Port() string {
+	return c.port
+}
+func (c *ServerConfig) Addr() string {
+	return c.addr
+}
+
+var config *ServerConfig
+
+func DefaultConfig() {
+	config = &ServerConfig{
+		protocol: os.Getenv("SERVER_PROTOCOL"),
+		host:	  os.Getenv("SERVER_HOST"),
+		port:	  os.Getenv("SERVER_PORT"),
+		addr: 	  os.Getenv("SERVER_HOST") + ":" + os.Getenv("SERVER_PORT"),
+	}
+}
