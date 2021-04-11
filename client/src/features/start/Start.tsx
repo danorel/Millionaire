ab806b6962f945d162e76b519d77be0977416743
danorel <mail.ordan@gmail.com>
2021-04-11 16:36:59 +0300

    [Routing + Layouts]: Configured the client basic web pages for the game.

---
+++ client/src/features/start/Start.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+type StartProps = {}
+
+const StartComponent: React.FC<StartProps> = () => {
+    return <React.Fragment></React.Fragment>
+}
+
+export default StartComponent
---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware")
+
+module.exports = function (app) {
+    app.use(
+        "/api",
+        createProxyMiddleware({
+            target: `${process.env.REACT_APP_SERVER_PROTOCOL}://${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}`,
+            changeOrigin: true
+        })
+    )
+}
