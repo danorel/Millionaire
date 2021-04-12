7dc804498a16178fbb0a6ad0ea5d0191e6f874a8
danorel <mail.ordan@gmail.com>
2021-04-12 15:25:20 +0300

    [Layouts]: Improved the FlexRow and FlexColumn components. Mobile responsiveness pre-ready stage.

--- client/src/components/layout/flex/FlexColumn/FlexColumn.tsx
+++ client/src/components/layout/flex/FlexColumn/FlexColumn.tsx
@@ -3,35 +3,12 @@ import styles from "./FlexColumn.module.css"
 
 type FlexColumnProps = {
     children: React.ReactNode
-    justifyContent?:
-        | "flex-start"
-        | "flex-end"
-        | "center"
-        | "space-between"
-        | "space-around"
-        | "space-evenly"
-        | "start"
-        | "end"
-        | "left"
-        | "right"
-        | "safe"
-        | "unsafe"
 }
 
 export const FlexColumn: React.FC<FlexColumnProps> = ({
-    children,
-    justifyContent = "center"
+    children
 }: FlexColumnProps) => (
     <React.Fragment>
-        <div
-            style={
-                {
-                    // justifyContent: justifyContent
-                }
-            }
-            className={styles.column}
-        >
-            {children}
-        </div>
+        <div className={styles.column}>{children}</div>
     </React.Fragment>
 )
