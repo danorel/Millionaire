ef93019bb5ce08e7887cba490cfa9951ce631092
danorel <mail.ordan@gmail.com>
2021-04-14 23:22:00 +0300

    [Package.json + Proxy]: Updating the proxy configurations. Fixing eslinted files.

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,12 @@
-const { createProxyMiddleware } = require('../node_modules/http-proxy-middleware');
+const {
+    createProxyMiddleware
+} = require("../node_modules/http-proxy-middleware")
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.use(
-        '/api',
+        "/api",
         createProxyMiddleware({
             target: `${process.env.REACT_APP_SERVER_PROTOCOL}://${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}`,
-            changeOrigin: true,
         })
-    );
-};
+    )
+}
