b2df441827c81dbd2d7787f440cf356a3e701baa
danorel <mail.ordan@gmail.com>
2021-04-15 03:02:46 +0300

    [Hooks]: Adding eslint hook + formatting on commit.

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
         createProxyMiddleware({
             target: `${process.env.REACT_APP_SERVER_PROTOCOL}://${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}`,
             secure: false,
-            changeOrigin: true,
+            changeOrigin: true
         })
     )
 }
